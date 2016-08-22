749f99cd80c4f798108c5ffd757986441774a3e6
jatwell93 <atwellj93@gmail.com>
2016-08-22 09:01:43 +0000

    forms and partials

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,46 @@
+class RecipesController < ApplicationController
+    def index
+        @recipes = Recipe.all
+    end
+    def show
+        @recipe = Recipe.find(params[:id])
+    end
+    
+    def new
+        @recipe = Recipe.new
+    end
+    def create
+        @recipe = Recipe.new(recipe_params)
+        @recipe.chef = Chef.find(1)
+        
+        if @recipe.save 
+            flash[:success] = "Your recipe was created succesfully!"
+            redirect_to recipes_path
+        else
+            render :new 
+        end
+    end
+    
+    def edit
+        @recipe = Recipe.find(params[:id])    
+    end
+    
+    def update 
+        @recipe = Recipe.find(params[:id])
+        if @recipe.update(recipe_params)
+           flash[:success] = "Your recipe was updated successfully!" 
+           redirect_to recipe_path(@recipe)
+        else 
+            render :edit
+        end
+    end
+    
+    
+    private 
+    
+        def recipe_params
+            params.require(:recipe).permit(:name, :summary, :description)
+        end
+    
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,16 @@ Rails.application.routes.draw do
     
   root "pages#home"
     
-  get "/home", to: "pages#home"
+  get "/home" => "pages#home"
+  
+  # get '/recipes' => 'recipes#index'
+  # get '/recipes/new' => 'recipes@new', as: 'new_recipe'
+  # post '/recipes' => 'recipes#create'
+  # get '/recipes/:id/edit' => 'recipes#edit', as: 'edit_recipe'
+  # patch '/recipes/:id' => 'recipes#update'
+  # get '/recipes/:id' => 'recipes#show', as: 'recipe'
+  # delete '/recipes/:id' => 'recipes#destroy'
+  
+  resources :recipes 
+  
 end
