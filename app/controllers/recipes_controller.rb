f952902677523570931c38dd0327771bfc48cbe2
Josh Atwell <atwellj93@gmail.com>
2016-09-07 07:21:14 +0000

    admin func.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,6 +3,7 @@ class RecipesController < ApplicationController
     before_action :require_user, except: [:show, :index, :like]
     before_action :require_user_like, only:[:like]
     before_action :require_same_user, only:[:edit, :update]
+    before_action :admin_user, only: :destroy 
     
     def index
         @recipes = Recipe.paginate(page: params[:page], per_page: 5)
@@ -50,6 +51,12 @@ class RecipesController < ApplicationController
        end
     end
     
+    def destroy
+       Recipe.find(params[:id]).destroy
+       flash[:success] = "Recipe Deleted"
+       redirect_to recipes_path
+    end
+    
     private 
     
     def recipe_params
@@ -61,7 +68,7 @@ class RecipesController < ApplicationController
     end
     
     def require_same_user
-        if current_user !=@recipe.chef
+        if current_user != @recipe.chef and !current_user.admin?
             flash[:danger] = "You can only edit your own recipes"
             redirect_to recipes_path
         end
@@ -73,4 +80,9 @@ class RecipesController < ApplicationController
           redirect_to :back
         end
     end
+    
+    def admin_user 
+       redirect_to recipes_path unless current_user.admin?
+    end
+    
 end
