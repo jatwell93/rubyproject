c347af7b30428691af1407a051f3e8bf90487280
Josh Atwell <atwellj93@gmail.com>
2016-10-07 02:07:42 +0000

    finsihed recipe part

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -20,10 +20,12 @@ class Recipe < ActiveRecord::Base
   															reject_if: proc { |attributes| attributes['step'].blank? },
   															allow_destroy: true
 
+    validates :directions, presence: true
+    validates :ingredients, presence: true
     validates :chef_id, presence: true
     validates :name, presence: true, length: { minimum: 5, maximum: 100}
     validates :summary, presence: true, length: { minimum: 10, maximum: 150}
-    validates :description, presence: true, length: { minimum: 20, maximum: 1000}
+    validates :description, presence: true, length: { minimum: 5, maximum: 1000}
     mount_uploader :picture, PictureUploader
     validate :picture_size 
     
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get '/home', to: 'pages#home'
-  
+  get '/about', to: 'pages#about'
+  get '/faq', to: 'pages#faq'
 
   resources :recipes do
     collection do
