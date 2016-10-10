1b4a5b9319689a2b518ca931fcd35b4ddb3797ee
Josh Atwell <atwellj93@gmail.com>
2016-10-10 07:04:52 +0000

    nothing of note

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,20 @@ module ApplicationHelper
         gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
         image_tag(gravatar_url, alt: chef.chefname, class: "gravatar")
     end
+    
+  def devise_error_mesages!
+    return '' if resource.errors.empty?
+    messages = resource.errors.full_messages.map {
+      |msg| content_tag(:li, msg)}.join
+    html = <<-HTML
+<div class="alert alert-error alert-danger">
+<a href='#' class='close' data-dismiss="alert"> &#215;</a>
+<%= content_tag :div , msg if msg.is_a(String) %>
+</div>
+HTML
+    html.html_safe
+  end
+
 end
 
 
