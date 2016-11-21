1307aa8a90b0f88b86dd5eeb3c47b19febe9e508
Josh Atwell <atwellj93@gmail.com>
2016-11-21 12:45:55 +0000

    finished comments for recipes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,18 @@
 class CommentsController < ApplicationController
 before_action :find_commentable
 
-    def new
-      @comment = Comment.new
-    end
+  def new
+    @comment = Comment.new
+  end
 
-    def create
-      @comment = @commentable.comments.new comment_params
-
-      if @comment.save
-        redirect_to :back, notice: 'Your comment was successfully posted!'
-      else
-        redirect_to :back, notice: "Your comment wasn't posted!"
-      end
+  def create
+    @comment = @commentable.comments.new(comment_params)
+    if @comment.save
+      redirect_to @commentable, notice: "Comment created."
+    else
+      render :new
     end
+  end
 
     private
 
@@ -23,7 +22,7 @@ before_action :find_commentable
 
     def find_commentable
       @commentable = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
-      @commentable = Recipe.find_by_id(params[:recipe_id]) if params[:recpe_id]
+      @commentable = Recipe.find_by_id(params[:recipe_id]) if params[:recipe_id]
     end
 
 end
