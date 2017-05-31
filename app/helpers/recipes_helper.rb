c90716606b3556c1d2cc17c17c4931df41dfc911
Mirv <reski.raptor@gmail.com>
2017-05-31 19:24:51 +0000

    formatting for reading, fixed error with user_id, made helper to remove some logic out of the way

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,9 +1,15 @@
 module RecipesHelper
-    def is_owner?
-      if current_user.id == @recipe.user_id then
-          return true
-      else
-          return false
-      end
+  def is_owner?
+    if current_user.id == @recipe.user_id then
+        return true
+    else
+        return false
     end
+  end
+  
+  def displayGravator?(user)
+    link_to gravatar_for(user, size: 100), user_path(user) unless user.nil?
+    
+  end
+  
 end
