dd234cb13711276730e8d2b36207364768f66eff
Mirv <reski.raptor@gmail.com>
2017-05-31 13:18:07 +0000

    Added valid & 3 invalid checks to match the recipe.rb validations

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -6,8 +6,22 @@ class RecipeTest < ActiveSupport::TestCase
   # end
 
   test "recipe is valid" do
-    recipe = recipes(:one)
-    assert recipe.invalid?, 'recipe is not valid'
+    recipe = Recipe.new(name: "SOMEONE", summary: "Something Else", description: "Something Else")
+    assert recipe.valid?, 'recipe is not valid'
   end
   
+  test "recipe should be invalid without description" do
+    recipe = Recipe.new(name: "Something", summary: "Something Else")
+    assert recipe.invalid?, 'recipe should be invalid, but passed validation'
+  end
+  
+  test "recipe should be invalid without summary" do
+    recipe = Recipe.new(name: "Something", description: "Something Else")
+    assert recipe.invalid?, 'recipe should be invalid, but passed validation'
+  end
+  
+  test "recipe should be invalid without name" do
+    recipe = Recipe.new(summary: "Something Else", description: "Something Else")
+    assert recipe.invalid?, 'recipe should be invalid, but passed validation'
+  end
 end
